1f3f65643d52046ec3f7327d627b25dbbdafb2b3
chenjian <chenjian@trophytech.net>
2016-06-12 00:58:04 +0800

    s

--- static/index.js
+++ static/index.js
@@ -43,3 +43,79 @@ $(document).ready(function() {
 
     });
 });
+
+
+$(function(){
+    function sortByTimes(a, b){
+        return b.count - a.count;
+    }
+    function sortByLen(a, b) {
+        return b.len - a.len;
+    }
+
+    var WORD_REGEXP = /(\b\w+\b)|[\u2E80-\u9FFF]/g;
+
+    function parse(text){
+        var word_arr = text.match(WORD_REGEXP);
+        var ret_arr = build(word_arr);
+        return ret_arr;
+    }
+    function build(in_arr){
+
+        function createItem(word){
+            var item = {};
+            item.count = 1;
+            item.word = word;
+            item.len = word.length;
+            return item;
+        }
+
+        var ret_arr = [];
+        var ret_obj = {};
+        $.each(in_arr, function(i, word){
+            if(!word) {
+                return;
+            }
+            var item = ret_obj[word];
+            if(item){
+                item.count++;
+            }else{
+                item = {
+                    count:1,
+                    word:word,
+                    len:word.length
+                };
+                ret_obj[word] = item;
+            }
+        });
+        $.each(ret_obj, function(i, obj){
+            ret_arr.push(obj);
+        });
+        return ret_arr;
+
+    }
+
+    $('#wordcount_btn').click(function(){
+        var source = $('#wordcount_input');
+        var val = source.val();
+        var arr = parse(val);
+
+        arr.sort(sortByTimes);
+
+        var str_arr = [];
+
+        for (var i = 0, len = arr.length; i < len; i++) {
+            var item = arr[i];
+            str_arr.push(item.word + ' x ' + item.count + ' <br>');
+        };
+
+        $('#wordcount_parse').html(str_arr.join(''));
+
+    });
+
+    $('#wordcount_save_btn').click(function(){
+
+    });
+
+});
+
